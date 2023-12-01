13565d253f39c6c03dc6c706c19579e371125fa2
bertinmick69 <bertinmick@yahoo.fr.com>
2023-12-01 12:19:29 +0100

    double js

--- Homepage/index.js
+++ Homepage/index.js
@@ -20,26 +20,3 @@ sousMenu.addEventListener("click", () => {
   sousMenu.style.display = "none";
   console.log("event", event);
 });
-
-const menu = document.querySelector(".fa-bars");
-const sousMenu = document.querySelector(".sous-menu");
-
-menu.addEventListener("click", () => {
-  if (sousMenu.style.display == "block") {
-    sousMenu.style.display = "none";
-  } else {
-    sousMenu.style.display = "block";
-  }
-  console.log("event", event);
-});
-
-const user = document.querySelector("#user");
-
-user.addEventListener("click", () => {
-  let users = prompt("tape ton pseudo");
-});
-
-sousMenu.addEventListener("click", () => {
-  sousMenu.style.display = "none";
-  console.log("event", event);
-});
